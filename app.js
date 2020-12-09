757d46806afe287059d4cede5d683043702762ac
saksham-tiwari <63906176+saksham-tiwari@users.noreply.github.com>
2020-12-09 22:53:02 +0530

    Added mouseOnCollide() function and comments

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var canvas, ctx, mx, my;
 var color = '#000000';
 var poly1, poly2,poly3,poly4;
-var n = Math.ceil(Math.random()*10);
-var m = Math.ceil(Math.random()*10);
-var l = Math.ceil(Math.random()*10);
+var n = Math.ceil(Math.random()*10); //random number for poly1
+var m = Math.ceil(Math.random()*10); //random number for poly3
+var l = Math.ceil(Math.random()*10); //random number for poly4
 
 
-
+// loads the canvas
 function load(){
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
@@ -26,7 +26,7 @@ window.addEventListener('mousemove', function(event){
   my = event.clientY; //y coordinate
 });
 
-//draw polygons
+//draw polygons and check for collision of two polygons
 function draw(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
   poly1 = polygon(canvas.width/2,canvas.height/2,80,n+3);
@@ -35,27 +35,15 @@ function draw(){
   poly4 = polygon((canvas.width*2)/3,canvas.height/2,70,l+3);
 
   if(collide(poly1,poly2)) {
-    ctx.beginPath();
-    ctx.arc(mx, my, 10, 0, 2*Math.PI)
-    ctx.strokeStyle = "#ffbf00";
-    ctx.lineWidth = 5;
-    ctx.stroke();
+    mouseOnCollide();
     color = "#FF6700";
   }
   else if(collide(poly3, poly2)){
-    ctx.beginPath();
-    ctx.arc(mx, my, 10, 0, 2*Math.PI)
-    ctx.strokeStyle = "#ffbf00";
-    ctx.lineWidth = 5;
-    ctx.stroke();
+    mouseOnCollide();
     color = "#ffffff";
   }
   else if (collide(poly4, poly2)){
-    ctx.beginPath();
-    ctx.arc(mx, my, 10, 0, 2*Math.PI)
-    ctx.strokeStyle = "#ffbf00";
-    ctx.lineWidth = 5;
-    ctx.stroke();
+    mouseOnCollide();
     color = "#BFFF00";
   }
   else {
@@ -63,8 +51,12 @@ function draw(){
   }
 }
 
+/*function which accepts
+start point coordinates as x,y
+radius as r and no. of sides as s
+to create new type of stroked polygon*/
 function polygon(x,y,r,s){
-  var a = Math.PI/2;
+  var a = Math.PI/(n+1);
   var points = [];
   var sides = [];
   ctx.beginPath();
@@ -87,6 +79,10 @@ function polygon(x,y,r,s){
   return {p:points, s:sides};
 }
 
+/*function which accepts
+start point coordinates as x,y
+radius as r and no. of sides as s
+to create new type of filled polygon for mouse pointer specifically*/
 function polygonMouse(x,y,r,s){
   var a = Math.PI/2;
   var points = [];
@@ -111,16 +107,7 @@ function polygonMouse(x,y,r,s){
   return {p:points, s:sides};
 }
 
-function collide(p1, p2){
-  for(var i in p1.s){
-    for(var j in p2.s){
-      var t = intersect(p1.s[i], p2.s[j]);
-      return t;
-    }
-  }
-}
-
-
+//function which calls intersect to check whether they really collide or not
 function collide(p1,p2) {
   for(var i in p1.s) {
     for(var j in p2.s) {
@@ -133,6 +120,8 @@ function collide(p1,p2) {
   }
   return false;
 }
+
+// function with algorithm to check collision of points of two polygons
 function intersect(s1,s2) {
   if(((s2[1].x - s2[0].x)*(s1[0].y - s1[1].y) - (s1[0].x - s1[1].x)*(s2[1].y - s2[0].y)) === 0) {
     return 'collinear';
@@ -142,6 +131,15 @@ function intersect(s1,s2) {
       tB =  ((s1[0].y - s1[1].y)*(s1[0].x - s2[0].x) + (s1[1].x - s1[0].x)*(s1[0].y - s2[0].y))/
             ((s2[1].x - s2[0].x)*(s1[0].y - s1[1].y) - (s1[0].x - s1[1].x)*(s2[1].y - s2[0].y));
   return [tA, tB];
+}
+
+//function to create a new circle on the mouse pointer
+function mouseOnCollide(){
+  ctx.beginPath();
+  ctx.arc(mx, my, 10, 0, 2*Math.PI)
+  ctx.strokeStyle = "#ffbf00";
+  ctx.lineWidth = 5;
+  ctx.stroke();
 }
 
 load();
