aa7646c6aafaeaddd6f50ed6de7634f1b3338524
saksham-tiwari <63906176+saksham-tiwari@users.noreply.github.com>
2020-12-09 21:21:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ window.addEventListener('mousemove', function(event){
 function draw(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
   poly1 = polygon(canvas.width/2,canvas.height/2,80,n+3);
-  poly2 = polygon(mx,my,5,4);
+  poly2 = polygonMouse(mx,my,5,4);
   poly3 = polygon(canvas.width/3,canvas.height/2,90,m+3);
   poly4 = polygon((canvas.width*2)/3,canvas.height/2,70,l+3);
 
@@ -40,7 +40,7 @@ function draw(){
     ctx.strokeStyle = "#ffbf00";
     ctx.lineWidth = 5;
     ctx.stroke();
-    color = "#ff0000";
+    color = "#FF6700";
   }
   else if(collide(poly3, poly2)){
     ctx.beginPath();
@@ -48,7 +48,7 @@ function draw(){
     ctx.strokeStyle = "#ffbf00";
     ctx.lineWidth = 5;
     ctx.stroke();
-    color = "#0000FF";
+    color = "#ffffff";
   }
   else if (collide(poly4, poly2)){
     ctx.beginPath();
@@ -56,7 +56,7 @@ function draw(){
     ctx.strokeStyle = "#ffbf00";
     ctx.lineWidth = 5;
     ctx.stroke();
-    color = "#008000";
+    color = "#BFFF00";
   }
   else {
     color = "#000000";
@@ -84,6 +84,30 @@ function polygon(x,y,r,s){
   ctx.strokeStyle = color;
   ctx.lineWidth = 1;
   ctx.stroke();
+  return {p:points, s:sides};
+}
+
+function polygonMouse(x,y,r,s){
+  var a = Math.PI/2;
+  var points = [];
+  var sides = [];
+  ctx.beginPath();
+  for(var i=0; i<=s; i++){
+    var px = x + r*Math.cos(a), py= y + r*Math.sin(a);
+    if(i===0){
+      ctx.moveTo(px,py);
+    }
+    else{
+      ctx.lineTo(px,py);
+      sides.push([{x: points[i-1].x, y: points[i-1].y},{x: px,y: py}])
+    }
+    points.push({x: px, y:py});
+    a += Math.PI*2/s;
+  }
+  points.pop(); //remove overlapping first and last points
+  ctx.fillStyle = "#ffffff";
+  ctx.lineWidth = 1;
+  ctx.fill();
   return {p:points, s:sides};
 }
 
